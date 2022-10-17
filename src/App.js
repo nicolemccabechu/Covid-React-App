b3266cc64d7dc3f0524e2afe1915b6d7e50714aa
Nicole <nicolemccabechu@gmail.com>
2022-10-17 15:37:32 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import FetchData from "./FetchData";
+import Header from "./Header";
+import React from 'react';
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <Navbar/>
+    <Header/>
+    <FetchData/>
+   </div>
   );
 }
 
